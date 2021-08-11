661ec2560b9a9409589221e71c77e52ad0f72ea5
KEEN <homurahomu@naver.com>
2021-08-11 15:23:34 +0900

    bubble 발사하기 및 발사할 bubble 랜덤 생성하기

--- bubble_shooter.py
+++ bubble_shooter.py
@@ -2,17 +2,38 @@
 # -*- coding:  utf-8 -*-
 
 # 발사대 겨냥 ( 방향키로 움직이기 )
-import os
+import os, random, math
 import pygame
 from pygame import image
 
 # 버블 클래스 생성
 class Bubble(pygame.sprite.Sprite):
-    def __init__(self, image, color, position):
+    def __init__(self, image, color, position = (0, 0)):
         super().__init__()
         self.image = image
         self.color = color
         self.rect = image.get_rect(center=position)
+        self.radius = 18 # 발사속도
+
+    def set_rect(self, position):
+        self.rect = self.image.get_rect(center=position)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def set_angle(self, angle):
+        self.angle = angle
+        self.rad_angle = math.radians(self.angle) # 호도법으로 변경한 값
+    
+    def move(self):
+        to_x = self.radius * math.cos(self.rad_angle)
+        to_y = self.radius * math.sin(self.rad_angle) * -1 # 방향을 위로 하기 위해서 -1 곱해주기.
+
+        self.rect.x += to_x
+        self.rect.y += to_y
+
+        if self.rect.left < 0 or self.rect.right > screen_width:
+            self.set_angle(180 - self.angle)
 
 # 발사대 클래스 생성
 class Pointer(pygame.sprite.Sprite):
@@ -95,6 +116,27 @@ def get_bubble_image(color):
     else:
         return bubble_images[-1]
 
+# 쏠 버블에 대한 준비를 하는 함수
+def prepare_bubbles():
+    global curr_bubble
+    curr_bubble = create_bubble() # 새 버블 생성 함수
+    curr_bubble.set_rect((screen_width // 2, 624))
+
+# 발사할 버블 랜덤으로 생성하는 함수
+def create_bubble():
+    color = get_random_bubble_color()
+    image = get_bubble_image(color)
+    return Bubble(image, color)
+
+# 랜덤으로 버블의 색을 골라주는 함수
+def get_random_bubble_color():
+    colors = []
+    for row in map:
+        for col in row:
+            if col not in colors and col not in [".", "/"]:
+                colors.append(col)
+    return random.choice(colors)
+
 pygame.init()
 
 screen_width = 448
@@ -135,6 +177,9 @@ to_angle_left = 0 # 좌측 각도 정보
 to_angle_right = 0 # 우측 각도 정보
 angle_speed = 1.5 # 1.5도씩 움직이게 된다.
 
+curr_bubble = None # 이번에 쏠 버블
+fire = False # 발사 여부 (현재 버블이 발사 중이면 발사되지 않도록하기 위해서.)
+
 map = [] # 게임 맵
 bubble_group = pygame.sprite.Group()
 
@@ -156,6 +201,10 @@ while running:
                 to_angle_left += angle_speed
             elif event.key == pygame.K_RIGHT: # 우측키 -> 화살표 각도가 점점 감소함.
                 to_angle_right -= angle_speed
+            elif event.key == pygame.K_SPACE: # 스페이스 -> 버블 발사
+                if curr_bubble and not fire:
+                    fire = True
+                    curr_bubble.set_angle(pointer.angle)
         
         # 위를 눌렀을 떄는 화살표의 이동이 멈춰야 하는 부분 설정.
         if event.type == pygame.KEYUP:
@@ -165,10 +214,23 @@ while running:
             elif event.key == pygame.K_RIGHT:
                 to_angle_right = 0
 
+    if not curr_bubble:
+        prepare_bubbles()
+
     screen.blit(background, (0, 0))
     bubble_group.draw(screen)
     pointer.rotate(to_angle_left + to_angle_right) # 발사대의 자연스러운 움직임을 위해서
     pointer.draw(screen)
+
+    if curr_bubble:
+        if fire:
+            curr_bubble.move() # 발사상태면 버블이 이동하도록.
+        curr_bubble.draw(screen)
+
+        if curr_bubble.rect.top <= 0: # 발사한 버블이 꼭대기에 닿으면 없어지고 새로운 버블이 생기도록.
+            curr_bubble = None
+            fire = False
+            
     pygame.display.update()
 
 pygame.quit()
