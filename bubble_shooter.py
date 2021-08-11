ffc9b197fcc176661fb933e4f352a08eef46b179
KEEN <homurahomu@naver.com>
2021-08-11 14:50:54 +0900

    Bubble 클래스 생성, map 만들기

--- bubble_shooter.py
+++ bubble_shooter.py
@@ -1,9 +1,36 @@
 # !/usr/lcoal/bin/python
 # -*- coding:  utf-8 -*-
 
-# 게임 화면 설정
+# 버블 이미지 설정 / map 설정
 import os
 import pygame
+
+# 버블 클래스 생성
+class Bubble(pygame.sprite.Sprite):
+    def __init__(self, image, color, position):
+        super().__init__()
+        self.image = image
+        self.color = color
+        self.rect = image.get_rect(center=position)
+
+# 맵 만들기
+# / : 버블이 들어갈 수 없는 공간을 의미.
+# . : 비어있는 공간.
+def setup():
+    global map
+    map = [
+        list("RRYYBBGG"),
+        list("RRYYBBG/"),
+        list("BBGGRRYY"),
+        list("BGGRRYY/"),
+        list("........"),
+        list("......./"),
+        list("........"),
+        list("......./"),
+        list("........"),
+        list("......./"),
+        list("........")
+    ]
 
 pygame.init()
 
