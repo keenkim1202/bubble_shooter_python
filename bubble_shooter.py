36c562942b9cc053ef3dbfeed33f6b01ae930f5e
KEEN <homurahomu@naver.com>
2021-08-11 14:44:29 +0900

    배경이미지 추가하기

--- bubble_shooter.py
+++ bubble_shooter.py
@@ -2,6 +2,7 @@
 # -*- coding:  utf-8 -*-
 
 # 게임 화면 설정
+import os
 import pygame
 
 pygame.init()
@@ -14,6 +15,10 @@ pygame.display.set_caption("Puzzle Bubble")
 
 clock = pygame.time.Clock()
 
+# 배경 이미지 불러오기
+current_path = os.path.dirname(__file__)
+background = pygame.image.load(os.path.join(current_path, "background.png"))
+
 running = True # running이 true일 동안은 계속 게임이 실행
 
 while running:
@@ -22,5 +27,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.blit(background, (0, 0))
+    pygame.display.update()
 
 pygame.quit()
